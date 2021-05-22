7e320e313f2ba70a79166dd763f425340a373fc5
Manoj Durairaj <MDURAIRAJ>
2021-05-22 12:50:07 +0000

    initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
+    return AppComponent.extend("com.suncor.zcbor2rloaddat.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
